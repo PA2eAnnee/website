43844b549dd8a316418b57c3040bbed3aaaed707
Janin-Michel-Mathias <micheljanin296@gmail.com>
2023-03-24 18:12:13 +0100

    initial commit

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Le h1 ments</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>le title ments</h1>
+    </body>
+</html>
